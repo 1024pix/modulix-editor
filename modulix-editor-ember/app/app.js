d8f1da00fcbebb29f43b43a31405a4a30fa92960
Eric Lim <eric.lim@zenika.com>
2026-08-25 15:58:51 +0200

    build: configure bootstrap and fontawesome
    by import the styles in app.js

--- modulix-editor-ember/app/app.js
+++ modulix-editor-ember/app/app.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 import Application from '@ember/application';
 import compatModules from '@embroider/virtual/compat-modules';
 import Resolver from 'ember-resolver';
